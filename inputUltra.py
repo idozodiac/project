581d4566dd470d835f87054720ef9f976e1b4cd5
idozodiac <77618069+idozodiac@users.noreply.github.com>
2021-10-12 22:04:18 +0300

    Add files via upload

---
+++ inputUltra.py
@@ -0,0 +1,33 @@
+import pygame, sys
+import serial
+
+ser = serial.Serial('COM3',9600)
+
+pygame.init()
+clock = pygame.time.Clock()
+screen = pygame.display.set_mode([800,800])
+base_font = pygame.font.Font(None,32)
+
+while True:
+    x = ser.readline()
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    screen.fill((0,0,0))
+    text_surface = base_font.render(x, True, (255,255,255))
+    screen.blit(text_surface,(0,0))
+
+    
+
+    pygame.display.flip()
+    clock.tick(60)
+
+    
+
+
+    
+    
+
+
---
+++ ultraserial.ino
@@ -0,0 +1,26 @@
+const int trigPin = 3;
+const int echoPin = 2;
+int incomingByte;
+
+
+float duration, distance_cm;
+
+void setup() {
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+  distance_cm = (duration*.0343)/2;
+  Serial.print("Distance: ");
+  Serial.println(distance_cm);
+  delay(1000);
+}
