41bfb59a58f49bd5003ca0ad25eae095c15af555
idozodiac <77618069+idozodiac@users.noreply.github.com>
2021-10-16 21:53:08 +0300

    Update inputUltra.py

--- inputUltra.py
+++ inputUltra.py
@@ -7,9 +7,14 @@ pygame.init()
 clock = pygame.time.Clock()
 screen = pygame.display.set_mode([800,800])
 base_font = pygame.font.Font(None,32)
+warning = "You are too close!"
+a = 'u good'
+
 
 while True:
     x = ser.readline()
+    t = str(x.decode().strip())
+    d = ('Distance is: ' + t + 'cm')
     y = float(x)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -17,19 +22,18 @@ while True:
             sys.exit()
 
     screen.fill((0,0,0))
-    text_surface = base_font.render(x, True, (255,255,255))
+    text_surface = base_font.render(d, True, (255,255,255))
     screen.blit(text_surface,(0,0))
 
-    if y < 6:
-        screen.fill((100,100,100))
-        text_surface = base_font.render(x, True, (255,255,255))
+    if y < 15:
+        screen.fill((255,0,0))
+        text_surface = base_font.render(d, True, (255,255,255))
+        text2 = base_font.render(warning, True, (255,255,255))
         screen.blit(text_surface,(0,0))
-
-
-    pygame.display.flip()
-    clock.tick(60)
-
-    
-    
-
-
+        screen.blit(text2,(300,300))
+    else:
+        text3 = base_font.render(a, True, (255,255,255))
+        screen.blit(text3,(300,300))
+        
+        
+        
